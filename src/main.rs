6701819c4e6b78157e87884e5770ae3da362c0e5
Nikita Lebedev <eazy-f@yandex.ru>
2018-04-16 20:33:16 +0300

    Read table record type

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::env;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::Result;
 
 static INDEX_EXT: &str = ".MYI";
 static DATA_EXT: &str = ".MYD";
 
 type MITableOptions = u64;
+
+const MI_OPTION_PACK_RECORD:     u64 = 1;
+const MI_OPTION_PACK_KEYS:       u64 = 2;
+const MI_OPTION_COMPRESS_RECORD: u64 = 4;
 
 struct MITableFiles {
     index: String,
@@ -20,7 +28,14 @@ trait Show {
 
 impl Show for MITableOptions {
     fn show(&self) -> String {
-        "unknown".to_string()
+        let record_type = if self & MI_OPTION_PACK_RECORD != 0 {
+            "packed"
+        } else if self & MI_OPTION_COMPRESS_RECORD != 0 {
+            "compressed"
+        } else {
+            "empty"
+        };
+        format!("{}", record_type.to_string())
     }
 }
 
@@ -29,8 +44,10 @@ fn main() {
     let directory = &args[1];
     let table_name = &args[2];
     let files = find_table_files(directory, table_name);
-    let header = read_table_header(files.index);
-    println!("{}", header.options.show());
+    match read_table_header(files.index) {
+        Ok(header) => println!("{}", header.options.show()),
+        Err(error) => println!("{}", error)
+    }
 }
 
 fn find_table_files(directory: &String, table_name: &String) -> MITableFiles {
@@ -42,8 +59,13 @@ fn find_table_files(directory: &String, table_name: &String) -> MITableFiles {
     }
 }
 
-fn read_table_header(index_file: String) -> MITableHeader {
-    MITableHeader {
-        options: 0x0
-    }
+fn read_table_header(index_file: String) -> Result<MITableHeader> {
+    let mut index = File::open(index_file)?;
+    let mut header_bytes = [0; 32];
+    index.read(&mut header_bytes)?;
+    let options = header_bytes[4..6].iter().fold(0, |acc, &b| acc*256 + b as u64);
+    let header = MITableHeader {
+        options: options
+    };
+    Ok(header)
 }
